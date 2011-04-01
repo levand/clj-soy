0d0d38cc62f1de944824a36d5aa52bdb3b9ff5cb
Luke VanderHart <luke@thinkrelevance.com>
2011-04-01 16:23:01 -0400

    Added clojars dev dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-soy "0.1.0"
   :description "A Clojure interface to the Google Closure templating library"
+  :dev-dependencies [[lein-clojars "0.6.0"]]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-soy/google-closure-templates "20100708"]])
